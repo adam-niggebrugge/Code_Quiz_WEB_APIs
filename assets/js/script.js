69dbee1bd103ef19620d8d6ecc43567561f17c5c
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-07-21 00:44:14 -0400

    20210721 js now registers on click events. CSS modified.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ var scoreFormEl = document.getElementById('scoreCollector');
 //set to global to be available in all functions
 var answer = "";
 var pointsLeft = 60;
+var isGameFinished = false;
+
 //Object to hold the questions, answer choices and the correct choice. 
 var questions = [
     {
@@ -28,7 +30,7 @@ var questions = [
     {
         title: "Inside which HTML element do we put the Javascript?",
         choices: ["<script>", "<scripting>", "<js>", "<javascript>"],
-        answer: "<script>"
+        answer: "&lt;script&gt;"
     },  
     {
         title: "What is the correct JavaScript syntax to change the content of the following HTML?  &lt;p id='demo'&gt;This is a demonstration.&lt;/p&gt;",
@@ -48,26 +50,33 @@ var questions = [
         choices: ["Both the <head> section and the <body> section are correct",
                 "The <head> section",
                 "The <body> section"],
-        answer: "Both the <head> section and the <body> section are correct"
+        answer: "Both the &lt;head&gt; section and the &lt;body&gt; section are correct"
     },  
 ];
+
+//used to get questions in a random order
+function randomOrder(object){
+    return Math.floor(Math.random() * object.length)
+}
 
 // Timer that counts down from 60
 function countdown() {
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
-        //always show the time left even after stopping
-        displayTimerEl.textContent = pointsLeft;
-        // As long as the `pointsLeft` is greater than 0 decrease the display
-        if (pointsLeft > 0) {
-            // Decrement `pointsLeft` by 1
-            pointsLeft--;
+        if(!isGameFinished){
+            //always show the time left even after stopping
+            displayTimerEl.textContent = pointsLeft;
+            // As long as the `pointsLeft` is greater than 0 decrease the display
+            if (pointsLeft > 0) {
+                // Decrement `pointsLeft` by 1
+                pointsLeft--;
+            } else {
+                endGame(); 
+                clearInterval(timeInterval);
+            } 
         } else {
-            // Use `clearInterval()` to stop the timer
             clearInterval(timeInterval);
-            // Call the `displayMessage()` function
-            endGame();
-        } 
+        }
     }, 1000);
   }
 
@@ -76,12 +85,14 @@ function renderQuestion(object) {
     dispayQuestionEl.innerHTML = "";
     dispayQuestionEl.innerHTML = object.title;
 }
+
 //This function will render choices in the object list and add data attributes to 'listen' if a user presses a key corresponding to multiplechoice
 function renderAnswerChoices(object){  
     for (var i = 0; i < object.choices.length; i++) {
         var choice = object.choices[i];  
         var li = document.createElement("li");
         li.textContent = choice;
+        li.setAttribute("class", "choice");
         li.setAttribute("data-index", i+1);
         li.setAttribute("id", 'choice'+String(i+1));
         answerChoicesEl.appendChild(li);
@@ -92,10 +103,89 @@ function clearAnswerChoices(){
    while(answerChoicesEl.firstChild){
     answerChoicesEl.removeChild(answerChoicesEl.firstChild);
    }
+   //this should clear the check answer with each new question
    checkAnswer = "";
 };
-function stopGame(){
 
+
+function answerResult(){
+    var resultTime = 1;  
+    var answerTimeInterval = setInterval(function () {
+        if(resultTime == 0)
+        {
+            displayResultEl.innerHTML = "";
+            displayResultEl.setAttribute("class", '');
+            clearInterval(answerTimeInterval);
+        }else if (resultTime > 0) {
+            resultTime--;
+        }
+    }, 1000);
+    getQuestion();
+}
+
+function reducePoints(){
+    if(pointsLeft > 10){
+        //pause for 3 seconds and reduce the points
+        pointsLeft = pointsLeft - 10;
+        displayTimerEl.textContent = pointsLeft;
+    }else{
+        pointsLeft = 0;
+        displayTimerEl.textContent = pointsLeft;
+        endGame();
+    }
+}
+
+function getQuestion(){
+    //check if there are any questions left to ask
+    if(questions.length >= 1){
+        clearAnswerChoices();
+        //create an empty object to temporary hold the indexed question pulled at random
+        var x = [];
+        //take a question at random from the established question objects, remove it from the set and place it in the temporary holder
+        x = questions.splice(randomOrder(questions), 1);
+        //send question to be displayed
+        renderQuestion(x[0]);
+        renderAnswerChoices(x[0]);
+        checkAnswer = x[0].answer;
+    }else{
+        stopGame();
+    }
+}
+
+function guessChecker(y){    
+    var evaluatedText = "";
+    //Check if value is coming from keyboard or click event
+    if(isNaN(y)){
+        evaluatedText = document.getElementById(y);
+    }else{
+        evaluatedText = document.getElementById('choice'+y);
+    }
+    if(evaluatedText.innerHTML == checkAnswer){
+        displayResultEl.innerHTML = "Correct!";
+        displayResultEl.setAttribute("class", 'right');
+        answerResult();
+    }else{
+        displayResultEl.innerHTML = "WRONG!";
+        displayResultEl.setAttribute("class", 'wrong');
+        reducePoints();
+        answerResult();     
+    }
+}
+
+function startQuiz(){
+    startButtonEl.style.display = 'none';
+    getQuestion();
+    countdown();    
+};
+
+function stopGame(){
+    //This is built in function that will end the point reduction/timer
+    isGameFinished = true;
+    var done = {
+        title: "You have answered all the questions!",
+    };
+    renderQuestion(done);
+    clearAnswerChoices();
 }
 
 //When the game ends due to wrong answers or no points left
@@ -107,70 +197,6 @@ function endGame(){
     clearAnswerChoices();
     return;
 };
-
-function reducePoints(){
-    displayResultEl.innerHTML = "WRONG!";
-    displayResultEl.setAttribute("class", 'wrong');
-    if(pointsLeft > 5){
-        //pause for 3 seconds and reduce the points
-        pointsLeft = pointsLeft - 2;
-        displayTimerEl.textContent = pointsLeft;
-        setTimeout(getQuestion(), 3000);  
-    }else{
-        endGame();
-    }
-}
-
-function startQuiz(){
-    startButtonEl.style.display = 'none';
-    getQuestion();
-    countdown();    
-};
-
-function randomOrder(object){
-    return Math.floor(Math.random() * object.length)
-}
-
-function getQuestion(){
-
-    //check if there are any questions left to ask
-    if(questions.length >= 1){
-    //remove any messages about being right or wrong as next question is shown
-    displayResultEl.innerHTML = "";
-    displayResultEl.setAttribute('class', '');
-    clearAnswerChoices();
-
-        //create an empty object to temporary hold the indexed question pulled at random
-        var x = [];
-        //take a question at random from the established question objects, remove it from the set and place it in the temporary holder
-        x = questions.splice(randomOrder(questions), 1);
-        //send question to be displayed
-    
-        renderQuestion(x[0]);
-        renderAnswerChoices(x[0]);
-        
-        checkAnswer = x[0].answer;
-        console.log(checkAnswer);
-    }else{
-        stopGame();
-    }
-
-}
-
-
-function guessChecker(y){
-    console.log(y);
-    var evaluatedText = document.getElementById('choice'+y);
-    if(evaluatedText.innerHTML == checkAnswer){
-        displayResultEl.innerHTML = "Correct!";
-        displayResultEl.setAttribute("class", 'right');
-        console.log('right');
-        getQuestion();
-    }else{
-        console.log('reduced points')
-        reducePoints();
-    }
-}
 
 startButtonEl.addEventListener("click", startQuiz);
 
@@ -184,10 +210,28 @@ document.addEventListener("keydown", function(event) {
     var key = event.key;
     var numericCharacters = "0123456789".split("");
     // Test if key pushed is a number
-    console.log('OUTSIDE event listner if condition, key pressed'+key) 
     if (numericCharacters.includes(key) && key <= answerChoicesEl.lastElementChild.dataset.index) {
-       console.log('inside event listner if, key is '+key) 
       var guess = event.key;
       guessChecker(guess);
+    }else {
+        displayResultEl.innerHTML = "Not a valid choice!";
+        displayResultEl.setAttribute("class", 'wrong');
     }
-  });
+});
+
+// TODO: Describe the purpose of the following line of code.
+answerChoicesEl.addEventListener("click", function(event) {
+    if (pointsLeft === 0) {
+        return;
+    }
+    var element = event.target;
+    // checks clicked element is a list item and then calls function to check answer.
+    if (element.matches("li") === true) {
+      var guess = element.getAttribute("id");
+      guessChecker(guess);
+    }
+});
+
+function recordName(){
+    
+};
